d2f737ec3791884b6d15079b3da40df0f5fc2016
Radu Andreea <andreea.radu154@gmail.com>
2023-12-09 00:27:23 +0200

    Add background image and home button

--- SmartGreenhouse/mobileApp/App.js
+++ SmartGreenhouse/mobileApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Home, CheckValues, Statistics, SystemOnOff } from "./src/screens";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,6 +7,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
+import backgroundImage from './assets/plants.webp'
 
 const Tab = createBottomTabNavigator()
 const screenOptions = {
@@ -25,79 +26,75 @@ const screenOptions = {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen 
-        name="Home" 
-        component={Home} 
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <View style={{alignItems: "center", justifyContent: "center"}}>
-                <FontAwesome name="home" size={24} color={focused ? "#16247d" : "#111"} />
-              </View>
-            )
-          }
-        }}
-        />
+    <View  style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <FontAwesome name="home" size={24} color={focused ? "#16247d" : "#111"} />
+                </View>
+              )
+            }
+          }}
+          />
 
-        <Tab.Screen 
-        name="CheckValues" 
-        component={CheckValues} 
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <View style={{alignItems: "center", justifyContent: "center"}}>
-                <MaterialCommunityIcons name="temperature-celsius" size={24} color={focused ? "#16247d" : "#111"} />
-              </View>
-            )
-          }
-        }}
-        />
+          <Tab.Screen 
+          name="CheckValues" 
+          component={CheckValues} 
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <MaterialCommunityIcons name="temperature-celsius" size={24} color={focused ? "#16247d" : "#111"} />
+                </View>
+              )
+            }
+          }}
+          />
 
-        <Tab.Screen 
-        name="Statistics" 
-        component={Statistics}
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <View style={{alignItems: "center", justifyContent: "center"}}>
-                <Entypo name="line-graph" size={24} color={focused ? "#16247d" : "#111"} />
-              </View>
-            )
-          }
-        }} 
-        />
+          <Tab.Screen 
+          name="Statistics" 
+          component={Statistics}
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <Entypo name="line-graph" size={24} color={focused ? "#16247d" : "#111"} />
+                </View>
+              )
+            }
+          }} 
+          />
 
-        <Tab.Screen 
-        name="SystemOnOff" 
-        component={SystemOnOff} 
-        options={{
-          tabBarIcon: ({focused}) => {
-            return (
-              <View style={{alignItems: "center", justifyContent: "center"}}>
-                <FontAwesome name="power-off" size={24} color={focused ? "#16247d" : "#111"} />
-              </View>
-            )
-          }
-        }}
-        />
+          <Tab.Screen 
+          name="SystemOnOff" 
+          component={SystemOnOff} 
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                  <FontAwesome name="power-off" size={24} color={focused ? "#16247d" : "#111"} />
+                </View>
+              )
+            }
+          }}
+          />
 
-      </Tab.Navigator>
-    </NavigationContainer>
-
-    // <View style={styles.container}>
-    //   <Text>Hello!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+        </Tab.Navigator>
+      </NavigationContainer>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
